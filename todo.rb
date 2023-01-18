57568acc0fed8a4cac82da93f4bea2f2492a3a90
Matt Lyell <mlyell21@Gmail.com>
2023-01-17 21:02:08 -0800

    Created views for creation of new todolists

--- todo.rb
+++ todo.rb
@@ -2,6 +2,40 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+
+configure do 
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do 
+  session[:lists] ||= []
+end 
+
 get "/" do
-  erb "You have no lists.", layout: :layout
+  redirect "lists"
+end
+
+# View all lists
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get "/lists/new" do 
+  erb :new_list, layout: :layout
+end
+
+# Create a new list
+post "/lists" do 
+  list_name = params[:list_name].strip
+  if (1..100).cover?(list_name.size)
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "A new list has been created"
+    redirect "/lists"
+  else
+    session[:error] = "List name must be between 1 and 100 characters."
+    erb :new_list, layout: :layout
+  end
 end
